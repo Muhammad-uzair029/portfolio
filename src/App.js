d00d1d0608a1a5f2b30d7842704ec5b0356786dd
muhammad-uzair029 <muhammaduzair0299@gmail.com>
2024-03-28 02:34:00 +0500

    Your commit message

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import MainBody from "./components/home/MainBody";
 import AboutMe from "./components/home/AboutMe";
 import Project from "./components/home/Project";
 import Footer from "./components/Footer";
-import Navbar from "./components/Navbar";
+// import Navbar from "./components/Navbar";
 import Skills from "./components/home/Skills";
 // import { Blog } from "./components/blog/Blog";
 // import BlogPost from "./components/blog/BlogPost";
